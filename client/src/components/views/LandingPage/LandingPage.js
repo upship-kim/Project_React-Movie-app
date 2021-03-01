8138f425e073af1094dbd011bcca78e12d892976
upship-kim <tkdqook@gmail.com>
2021-03-01 18:58:38 +0900

    Ant Design setup

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 function LandingPage() {
 
     useEffect(() => { //LandingPage가 실행되자마자 실행되는 기능 
-        axios.get('/api/hello')
-        .then(response =>console.log(response.data));
+        axios.get('/api/hello') //server단에 route 발생 시킨다. 
+        .then(response =>console.log(response.data));   //res가 오면 콘솔로 res의 data를 출력시킨다. 
     }, [])
 
     return (
