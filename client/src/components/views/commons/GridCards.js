a9cb813ebc6fe523eaee9eedb2ad1da703f32ed2
upship-kim <tkdqook@gmail.com>
2021-05-27 12:32:54 +0900

    success function toggle

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -4,33 +4,37 @@ import {Col} from 'antd';
 const GridCards = ({image, movieId, movieName, cursor}) => {
     if (!cursor) {
         return (
-            <Col lg={6} md={8} xs={24}>
+            <>
                 {image && (
-                    <div style={{position: 'relative'}}>
-                        <img
-                            src={image}
-                            alt={movieName}
-                            style={{width: '100%', height: '100%'}}
-                        />
-                    </div>
+                    <Col lg={6} md={8} xs={24}>
+                        <div style={{position: 'relative'}}>
+                            <img
+                                src={image}
+                                alt={movieName}
+                                style={{width: '100%', height: '100%'}}
+                            />
+                        </div>
+                    </Col>
                 )}
-            </Col>
+            </>
         );
     }
     return (
-        <Col lg={6} md={8} xs={24}>
+        <>
             {image && (
-                <div style={{position: 'relative'}}>
-                    <a href={`/movie/${movieId}`}>
-                        <img
-                            src={image}
-                            alt={movieName}
-                            style={{width: '100%', height: '100%'}}
-                        />
-                    </a>
-                </div>
+                <Col lg={6} md={8} xs={24}>
+                    <div style={{position: 'relative'}}>
+                        <a href={`/movie/${movieId}`}>
+                            <img
+                                src={image}
+                                alt={movieName}
+                                style={{width: '100%', height: '100%'}}
+                            />
+                        </a>
+                    </div>
+                </Col>
             )}
-        </Col>
+        </>
     );
 };
 
